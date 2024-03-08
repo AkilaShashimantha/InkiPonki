c59f1f832940519602d3af50045a794b0764d7ec
AkilaShashimantha <125176122+AkilaShashimantha@users.noreply.github.com>
2024-03-08 19:11:49 +0530

    create an object called Ponki inside the Inki class and make local variable called pinki and it's value id donki print variable pinki inside the constructor

---
+++ Inki.java
@@ -0,0 +1,20 @@
+public class Inki{
+
+
+
+public static void main(String[] args){
+
+String pinki = "donki";    
+
+Inki ponki = new Inki(pinki);
+
+}
+
+public Inki(String a){
+
+System.out.println(a);
+
+}
+
+
+}
